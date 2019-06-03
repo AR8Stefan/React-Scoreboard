5e38628c2a3c494c0885bc4e9278cb6f269a8834
AR8Stefan <stefan.piatkov@gmail.com>
2019-06-03 15:16:58 -0500

    Teacher Challenge: Make Counter its own component and display it with the Player component (Before watching the video)

--- app.js
+++ app.js
@@ -14,13 +14,19 @@ const Player = () => {
                 Stefan
             </span>
 
-            <div className="counter">
-                <button className="counter-action decrement"> - </button>
-                <span className="counter-score">1</span>
-                <button className="counter-action increment"> + </button>
-            </div>
+            <Counter />
         </div>
-    )
+    );
+}
+
+const Counter = () => {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">1</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
 }
 
 ReactDOM.render(
